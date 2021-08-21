fbf80c69cd547422dd39d3e32aec93634ca694ae
GarenMelikyan <garenmelikyan@gmail.com>
2021-08-21 15:24:41 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+ReactDOM.render(
+  <div>
+    <h1>Armenia</h1>
+
+    <h2>Yerevan</h2>
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Mount_Ararat_and_the_Yerevan_skyline_in_spring_from_the_Cascade.jpg/1280px-Mount_Ararat_and_the_Yerevan_skyline_in_spring_from_the_Cascade.jpg"
+      alt="avatar_img"
+    />
+
+    <p>Capital</p>
+
+    <h2>Gyumri</h2>
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/2014_Prowincja_Szirak%2C_Giumri%2C_Plac_Vartanants_%2802%29.jpg/800px-2014_Prowincja_Szirak%2C_Giumri%2C_Plac_Vartanants_%2802%29.jpg"
+      alt="avatar_img"
+    />
+    <p>Shirak Marz</p>
+
+    <h2>Berd</h2>
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/%D4%B2%D5%A5%D6%80%D5%A4%2C_%D5%8F%D5%A1%D5%BE%D5%B8%D6%82%D5%B7_%282%29.JPG/1280px-%D4%B2%D5%A5%D6%80%D5%A4%2C_%D5%8F%D5%A1%D5%BE%D5%B8%D6%82%D5%B7_%282%29.JPG"
+      alt="avatar_img"
+    />
+    <p>Tavush Marz</p>
+  </div>,
+  document.getElementById("root")
+);
